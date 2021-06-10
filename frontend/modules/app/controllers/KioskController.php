4a615a5aee8f2b15d6afe3e59b07304f06a46652
admin <pun.bxxx@gmail.com>
2021-06-10 17:17:09 +0700

    #pun //bug

--- frontend/modules/app/controllers/KioskController.php
+++ frontend/modules/app/controllers/KioskController.php
@@ -452,7 +452,7 @@ class KioskController extends \yii\web\Controller
       WHERE
         q_status_id = 1
         AND serviceid = :serviceid
-        AND q_ids < q.q_ids
+        AND q_ids < tb_quequ.q_ids
         AND DATE( tb_quequ.q_timestp ) = CURRENT_DATE
       ) AS queue_left';
     $params = [':serviceid' => $model['serviceid'], ':q_ids' => $id];
