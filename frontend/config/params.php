232c3d5d9959571c1046a288db46cfac84f249bc
admin <pun.bxxx@gmail.com>
2021-08-05 15:15:46 +0700

    change config

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'messageURL' => YII_ENV_DEV ? 'http://localhost:3002/api/add-message' : 'http://qstd3.andamandev.com/node3/api/add-message' 
+    'messageURL' => YII_ENV_DEV ? 'http://localhost:3003/api/add-message' : 'http://qstd3.andamandev.com/node/api/add-message' 
 ];
